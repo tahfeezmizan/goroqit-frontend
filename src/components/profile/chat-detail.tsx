03c2fd3244fc453b8a54c816f2bd237f5b1e8481
tahfeezmizan <mizanahmed5316@gmail.com>
2025-12-10 05:59:11 +0600

    udpate chat detials and chat list

--- src/components/profile/chat-detail.tsx
+++ src/components/profile/chat-detail.tsx
@@ -214,7 +214,7 @@ export default function ChatDetail() {
   }
 
   return (
-    <div className="flex-1 flex flex-col bg-white min-h-[calc(100vh-128px)]">
+    <div className="flex-1 w-full flex flex-col bg-white min-h-[calc(100vh-128px)]">
       {/* Header */}
       <div className="bg-white border-b border-gray-200 p-3 flex items-center justify-between flex-shrink-0">
         <div className="flex items-center">
